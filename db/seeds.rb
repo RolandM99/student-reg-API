88a28668c364db32973fdb1f45fa77e93ea87479
rolandm99 <manfulmweze99@gmail.com>
2022-06-03 07:44:05 +0100

    Generate migration file and schema

---
+++ db/seeds.rb
@@ -0,0 +1,12 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+#   Character.create(name: "Luke", movie: movies.first)
+
+Student.create(student_id: 24288, first_name: "Roland", last_name: "Mweze", age: 23, gender: "Male", academic_year: "2021-2022")
+Student.create(student_id: 24228, first_name: "Tite", last_name: "Lunda", age: 24, gender: "Male", academic_year: "2021-2022")
+Student.create(student_id: 24288, first_name: "Eliezer", last_name: "Bulande", age: 24, gender: "Male", academic_year: "2021-2022")
+Student.create(student_id: 21288, first_name: "Kandy", last_name: "Kamuntu", age: 22, gender: "Male", academic_year: "2021-2022")
